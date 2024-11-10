70ca758473ea700341243a4af69625f60a49fca8
heskirulitaa <heskirulitapratiwi57@gmail.com>
2024-11-10 23:14:36 +0700

    update2

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-document.querySelectorAll('.question-box h5').forEach(item => {
-    item.addEventListener('click', () => {
-        item.classList.toogle('active');
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
 
+document.getElementById("signIn-button").addEventListener("click", function() {
+    window.location.href = 'signIn.html';
+});
